f7f3b9150cad2e7f133e1e06d1e4e89d0dfc0a81
nunes6786 <arthurnunes6786@gmail.com>
2025-12-03 10:29:37 -0300

    Update cobra.js

--- cobra.js
+++ cobra.js
@@ -1,21 +1,35 @@
 const cobra = {
-    vida: 3, 
-    tamanho: 10,
-    x: 400,
-    y: 260,
-    cor: 'brown', 
-    direcao: 'right',
+    vida: 3,
+    tamanho:10,
+    x:400,
+    y:260,
+    cor:'brown',
+    direcao:'right',
     desenhar(){
-        cCtx.fillStyle = this.cor
-        cCtx.fillRect(this.x, this.y, this.tamanho, this. tamanho)
+        cCtx.fillStyle = this.cor;
+        cCtx.fillRect(this.x,this.y,this.tamanho,this.tamanho);
     },
     mover(){
-        if (this.direcao == 'right') this.x++;
-        if (this.direcao == 'left') this.x--;
-        if (this.direcao == 'down') this.y++;
-        if (this.direcao == 'up') this.y--;
+      if (this.direcao == "right")this.x++;
 
+      if (this.direcao == "left")this.x--;
 
-    }
+      if (this.direcao == "down")this.y++;
+      
+      if (this.direcao == "up")this.y--;
+      
+      if((this.x >= tela.largura - this.tamanho) || (this.x <= 0)) 
+      this.morrer();
 
+      if((this.y >= tela.altura + placar.altura - this.tamanho) || (this.y <= placar.altura)) 
+      this.morrer();
+
+      
+    },
+morrer(){
+  this.vida--;
+  this.x=400;
+  this.y=260;
 }
+}
+
